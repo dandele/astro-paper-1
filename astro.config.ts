db583c013713f9c70057ac518f8eefec6dfd2848
dandele <dandele93@gmail.com>
2023-12-25 03:06:56 +0100

    riproviamo

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,10 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +19,16 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "us", // optional,  or 'eu' (default)
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [
